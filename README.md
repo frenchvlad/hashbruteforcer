3bce66ca7f553d53505374a72157a4d20660ceff
frenchvlad <71971673+frenchvlad@users.noreply.github.com>
2020-09-27 14:40:37 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# HashDictio (using rockyou.txt)
+HashDictio - Dictionnary Attack in Python
+
+# MD5 with the hash (a425352a84b14c7acb601495bd156322)
+<img src="https://i.imgur.com/ofz71ul.png"/>
+
+# SHA1 with the hash (5fee00239940f883d4c2854e41c7f989e75278a3)
+<img src="https://i.imgur.com/WIPru6o.png"/>
---
+++ hashdictio.py
@@ -0,0 +1,43 @@
+#coding: utf-8
+import hashlib
+"""
+made by frenchvlad from paris
+"""
+def main():
+    with open("rockyou.txt") as f:
+        #for line in f:
+            #line = line.strip()
+        algorithm = input("Select the algorithm (md5/sha1) : ")
+        if algorithm == "md5" or algorithm == "MD5":
+            bb = input("Enter the hash (len = 32) : ")
+            if len(bb) == 32:
+                print("\n")
+                for line in f:
+                    line = line.strip()
+                    hashes = hashlib.md5(line.encode('utf-8')).hexdigest()
+                    if hashes == bb:
+                        print(" [+] The hash matches the password : "+str(line))
+                        exit()
+                    else:
+                        print(" [-] Hash is not : "+str(line))
+            else:
+                print("Wrong hash.")
+                exit()
+        if algorithm == "sha1" or algorithm == "SHA1":
+            bb = input("Enter the hash (len = 40) : ")
+            if len(bb) == 40:
+                print("\n")
+                for line in f:
+                    line = line.strip()
+                    hashes = hashlib.sha1(line.encode('utf-8')).hexdigest()
+                    if hashes == bb:
+                        print(" [+] The hash matches the password : "+str(line))
+                        exit()
+                    else:
+                        print(" [-] Hash is not : "+str(line))
+            else:
+                print("Wrong hash.")
+                exit()
+        else:
+            exit()
+main()
